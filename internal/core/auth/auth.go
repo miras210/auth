2656064f5727de7d61844261060a9d71761c1c23
altercolt <aybarrel@gmail.com>
2022-10-18 01:56:59 +0600

    Initial structures

--- internal/core/auth/auth.go
+++ internal/core/auth/auth.go
@@ -1,1 +1,38 @@
 package auth
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+const (
+	AccessTokenExpiration  = time.Minute * 15
+	RefreshTokenExpiration = time.Hour * 24 * 7
+)
+
+// Payload
+// is the claims part of jwt token
+// stored in context
+type Payload struct {
+	UserID int    `json:"user_id"`
+	Role   string `json:"role"`
+	Exp    int64  `json:"exp"`
+}
+
+// RefreshToken
+// is model stored in the database
+type RefreshToken struct {
+	ID             uuid.UUID `json:"id"`
+	UserID         int       `json:"user_id"`
+	RefreshToken   string    `json:"refresh_token"`
+	ExpirationTime time.Time `json:"expiration_time"`
+}
+
+// TokenPair
+// is the structure that is returned to the user as JSON
+type TokenPair struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+	AccessExp    int64  `json:"access_token_exp"`
+	RefreshExp   int64  `json:"refresh_token_exp"`
+}
--- internal/core/user/user.go
+++ internal/core/user/user.go
@@ -1,1 +1,23 @@
 package user
+
+type User struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	PassHash string `json:"password"`
+}
+
+// NewUser
+// is used for creating new user record in the database
+type NewUser struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+// UpdateUser
+// not sure about internals yet, but it is used for updating user data
+type UpdateUser struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
